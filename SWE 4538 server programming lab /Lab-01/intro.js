3b27bc71b6225ee2cb47dac43b39b25d29052371
Azmayen Fayek Sabil <azmayenfayek@iut-dhaka.edu>
2022-08-05 12:28:23 +0600

    server programming lab first lab

---
+++ SWE 4538 server programming lab /Lab-01/intro.js
@@ -0,0 +1,63 @@
+const {generateRandom, generateRandom2} = require('./utils');
+// console.log('Hello world!');
+
+// let num = 5;
+// console.log('num: ' + num);
+
+// const num2 = 6;
+// console.log('num2: ' + num2);
+
+// //function --> lambda function
+// const Hello = (name) => {
+//     console.log(name);
+// }
+
+// Hello('sabil');
+
+
+// const checkEven = (num) => {
+//     if(num%2 == 0)  console.log('even');
+//     else console.log('odd');
+// }
+
+// const checkEven1 = (num) => {
+//     if(num%2 == 0)  return 'even';
+//     else return 'odd';
+// }
+
+// const checkEven2 = (min, max) => {
+//     let num = Math.floor(Math.random() * (max-min));
+//     console.log(num);
+
+//     if(num%2 == 0)  console.log('even');
+//     else console.log('odd');
+// }
+
+// checkEven(11); 
+
+// console.log(checkEven1(10));
+
+// importing library
+
+// const os = require('os');
+// console.log(os.totalmem());
+
+// odd even advanced functions
+
+// let min = 10;
+// let max = 100;
+
+// checkEven2(min,max);
+
+// JSON -- javascript object notations
+
+// const sampleJson = 
+// {
+//     name: 'sabile',
+//     age: 22
+// }
+
+// console.log(sampleJson);
+
+generateRandom();
+generateRandom2();  
---
+++ SWE 4538 server programming lab /Lab-01/utils.js
@@ -0,0 +1,9 @@
+const generateRandom = () => {
+    console.log('Generating random number generator: ' + Math.random());
+}
+
+const generateRandom2 = () => {
+    console.log('Generating random number generator: ' + Math.random());
+}
+
+module.exports = {generateRandom, generateRandom2};
